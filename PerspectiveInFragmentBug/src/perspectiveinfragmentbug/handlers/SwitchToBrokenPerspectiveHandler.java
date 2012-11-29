d4537eed4a9f441dc09002c280e84177d26919ea
Wolfy <halbeisen@hotmail.com>
2012-11-29 10:48:44 +0100

    added example-project to reproduce the bug

---
+++ PerspectiveInFragmentBug/src/perspectiveinfragmentbug/handlers/SwitchToBrokenPerspectiveHandler.java
@@ -0,0 +1,24 @@
+package perspectiveinfragmentbug.handlers;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+
+public class SwitchToBrokenPerspectiveHandler {
+
+	@Inject private EPartService partService;
+	@Inject private EModelService modelService;
+	@Inject private MApplication application;
+	
+	@Execute
+	public void execute() {
+		
+		MPerspective perspective = (MPerspective) modelService.find("perspective.broken", application);
+		
+		partService.switchPerspective(perspective);
+	}
+}
